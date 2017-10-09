c0fc30489d2a1c17044927662b449e3546bf80dd
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-09 15:57:39 -0700

    Cleanup

--- ratings.py
+++ ratings.py
@@ -22,21 +22,48 @@ def print_ratings(dictionary):
     """ Sorts and prints the contents of the dictionary.
     """
 
+    print
     # Loops through dict and prints sorted, formatted key-value pairs
     for r_name, r_rating in sorted(dictionary.items()):
         print '{} is rated at {}.'.format(r_name, r_rating)
 
 
-def add_restaurant_rating():
+def add_restaurant_rating(rest_dict):
     """ User inputs restaurant name and rating, adds to existing dictionary.
 
     """
-
     restaurant_name = raw_input("Enter the restaurant name to rate: ")
-    restaurant_rating = raw_input("Enter the rating (1 to 5) for the restaurant: ")
+    while True:
+        restaurant_rating = raw_input("Enter the rating (1 to 5) for the restaurant: ")
+        if restaurant_rating in '12345':
+            break
+        else:
+            print "Sorry we only accept integers between 1 and 5 inclusively."
+
     rest_dict[restaurant_name] = restaurant_rating
 
 
-rest_dict = alphabetize_ratings_from_disk('scores.txt')
-add_restaurant_rating()
-print_ratings(rest_dict)
+def decide_user_actions():
+    """ Processes data; user decides to view, add, or quit.
+    """
+
+    print
+    rest_dict = alphabetize_ratings_from_disk('scores.txt')
+
+    while True:
+        decision = raw_input(
+            "Press 'V' to View the list, "
+            "'A' to Add to the list, "
+            "or 'Q'" "to quit. ")
+        if decision.upper() == 'Q':
+            break
+        elif decision.upper() == 'V':
+            print_ratings(rest_dict)
+        elif decision.upper() == 'A':
+            add_restaurant_rating(rest_dict)
+        else:
+            print "I don't understand you. Please try again."
+
+        print
+
+decide_user_actions()
